0869cf776d0099cee472617018a6cdcb83133fa4
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-02-09 22:36:00 +0100

    TypeScript fully configured environment

---
+++ src/ts-sample/sample-file.ts
@@ -0,0 +1,27 @@
+/**
+ * This is just a sample file to test if the TypeScript compiler work!
+ */
+
+interface ILogger {
+    log(message: string): void;
+}
+
+class SampleLogger implements ILogger {
+
+    /**
+     * will be transpiled to a prototype function
+     */
+    log(message: string): void {
+        console.log(message);
+    }
+
+    /**
+     * will be transpiled to a member function
+     */
+    log2 = (message: string): void => {
+        console.log(message);
+    }
+}
+
+let logger = new SampleLogger();
+logger.log("log message");
